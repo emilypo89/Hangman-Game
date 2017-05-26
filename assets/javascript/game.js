fc5ba7628dcb442056dc59747338ce205d225934
emilypo89 <emilypo89@gmail.com>
2017-05-26 09:52:25 -0400

    comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,6 @@
-// start program
 
-// show random word on screen
-
-function hangman(){
-	// document.getElementByClass(".lifeImages").style.visibility = "hidden"
-
-	var words = ["horse", "radio", "mouse", "money"];
+// global variables 
+var words = ["horse", "radio", "mouse", "money"];
 	var generatedWord = words[Math.floor(Math.random() * words.length)];
 	var incorrectGuess = 0;
 	var lives = 6;
@@ -13,220 +8,223 @@ function hangman(){
 	var losses = 0;
 	var displayWord = "";
 	var correctLetters = [];
-	
 
-// Input letters
-document.onkeyup = function(event) {
-	var userGuess = event.key;
+// need to push "push to play" button for game to work
+function hangman(){
 
-if (generatedWord === words[0]){
-	 	var lettersHorse = ["h", "o", "r", "s", "e"];
-	 	for(i = 0; i < lettersHorse.length; i++) {
-		 		console.log(lettersHorse[i]);
+	// Input letters
+	document.onkeyup = function(event) {
+		var userGuess = event.key;
+	// generated word "horse"
+	if (generatedWord === words[0]){
+		 	var lettersHorse = ["h", "o", "r", "s", "e"];
+		 	for(i = 0; i < lettersHorse.length; i++) {
+			 		console.log(lettersHorse[i]);
 
-		if (userGuess === lettersHorse[0]){
-			var horseLetter0 = document.getElementById("div0");
-			horseLetter0.innerHTML = "h";
-			letterCounter++;
+			if (userGuess === lettersHorse[0]){
+				var horseLetter0 = document.getElementById("div0");
+				horseLetter0.innerHTML = "h";
+				letterCounter++;
+			}
+			else if (userGuess === lettersHorse[1]){
+				var horseLetter1 = document.getElementById("div1");
+				horseLetter1.innerHTML = "o";
+				letterCounter++;
+			}
+
+			else if (userGuess === lettersHorse[2]){
+				var horseLetter2 = document.getElementById("div2");
+				horseLetter2.innerHTML = "r";
+				letterCounter++;
+			}
+
+			else if (userGuess === lettersHorse[3]){
+				var horseLetter3 = document.getElementById("div3");
+				horseLetter3.innerHTML = "s";
+				letterCounter++;
+			}
+			else if (userGuess === lettersHorse[4]){
+				var horseLetter4 = document.getElementById("div4");
+				horseLetter4.innerHTML = "e";
+				letterCounter++;
+			}
+
+		 	else if (userGuess !== lettersHorse[i]){
+		 		incorrectGuess++;
+		 		lives--;
+		 	}
+
 		}
-		else if (userGuess === lettersHorse[1]){
-			var horseLetter1 = document.getElementById("div1");
-			horseLetter1.innerHTML = "o";
-			letterCounter++;
-		}
+		 	if (letterCounter === 5){
+		 		wins++;
+		 		reset();
+		 	}
 
-		else if (userGuess === lettersHorse[2]){
-			var horseLetter2 = document.getElementById("div2");
-			horseLetter2.innerHTML = "r";
-			letterCounter++;
-		}
-
-		else if (userGuess === lettersHorse[3]){
-			var horseLetter3 = document.getElementById("div3");
-			horseLetter3.innerHTML = "s";
-			letterCounter++;
-		}
-		else if (userGuess === lettersHorse[4]){
-			var horseLetter4 = document.getElementById("div4");
-			horseLetter4.innerHTML = "e";
-			letterCounter++;
-		}
-
-	 	else if (userGuess !== lettersHorse[i]){
-	 		incorrectGuess++;
-	 		lives--;
-	 	}
-
+		 	if (lives = 0){
+		 		losses++;
+		 		reset();
+		 	}
 	}
-	 	if (letterCounter === 5){
-	 		wins++;
-	 		reset();
-	 	}
+		// generated word "radio"
+		if (generatedWord === words[1]){
+		 	var lettersRadio= ["r", "a", "d", "i", "o"];
+		 	for(i = 0; i < lettersRadio.length; i++) {
+			 		console.log(lettersRadio[i]);
 
-	 	if (lives = 0){
-	 		losses++;
-	 		reset();
-	 	}
+			if (userGuess === lettersRadio[0]){
+				var radioLetter0 = document.getElementById("div0");
+				radioLetter0.innerHTML = "r";
+			}
+			else if (userGuess === lettersRadio[1]){
+				var radioLetter1 = document.getElementById("div1");
+				radioLetter1.innerHTML = "a";
+			}
+
+			else if (userGuess === lettersRadio[2]){
+				var radioLetter2 = document.getElementById("div2");
+				radioLetter2.innerHTML = "d";
+			}
+
+			else if (userGuess === lettersRadio[3]){
+				var radioLetter3 = document.getElementById("div3");
+				radioLetter3.innerHTML = "i";
+			}
+			else if (userGuess === lettersRadio[4]){
+				var radioLetter4 = document.getElementById("div4");
+				radioLetter4.innerHTML = "o";
+				letterCounter++;
+			}
+		 	else if (userGuess !== lettersRadio[i]){
+		 		incorrectGuess++;
+		 		lives--;
+		 	}
+
+		}
+		 	if (letterCounter === 5){
+		 		wins++;
+		 		reset();
+		 	}
+
+		 	if (lives = 0){
+		 		losses++;
+		 		reset();
+		 	}
+	}
+
+		// generated work "mouse"
+		if (generatedWord === words[2]){
+		 	var lettersMouse = ["m", "o", "u", "s", "e"];
+		 	for(i = 0; i < lettersMouse.length; i++) {
+			 		console.log(lettersMouse[i]);
+			 		letterCounter++;
+
+			if (userGuess === lettersMouse[0]){
+				var mouseLetter0 = document.getElementById("div0");
+				mouseLetter0.innerHTML = "m";
+				letterCounter++;
+			}
+			else if (userGuess === lettersMouse[1]){
+				var mouseLetter1 = document.getElementById("div1");
+				mouseLetter1.innerHTML = "0";
+				letterCounter++;
+			}
+
+			else if (userGuess === lettersMouse[2]){
+				var mouseLetter2 = document.getElementById("div2");
+				mouseLetter2.innerHTML = "u";
+				letterCounter++;
+			}
+
+			else if (userGuess === lettersMouse[3]){
+				var mouseLetter3 = document.getElementById("div3");
+				mouseLetter3.innerHTML = "s";
+				letterCounter++;
+			}
+			else if (userGuess === lettersMouse[4]){
+				var mouseLetter4 = document.getElementById("div4");
+				mouseLetter4.innerHTML = "e";
+				letterCounter++;
+			}
+
+		 	else if (userGuess !== lettersMouse[i]){
+		 		incorrectGuess++;
+		 		lives--;
+		 	}
+
+		 }
+
+		 	if (letterCounter === 5){
+		 		wins++;
+		 		reset();
+		 	}
+
+		 	if (lives = 0){
+		 		losses++;
+		 		reset();
+		 	}
+		}
+		// generated word "money"
+		if (generatedWord === words[3]){
+		 	var lettersMoney = ["m", "o", "n", "e", "y"];
+		 	for(i = 0; i < lettersMoney.length; i++) {
+			 		console.log(lettersMoney[i]);
+
+			if (userGuess === lettersMoney[0]){
+				var moneyLetter0 = document.getElementById("div0");
+				moneyLetter0.innerHTML = "m";
+				letterCounter++;
+
+			}
+			else if (userGuess === lettersMoney[1]){
+				var moneyLetter1 = document.getElementById("div1");
+				moneyLetter1.innerHTML = "o";
+				letterCounter++;
+			}
+
+			else if (userGuess === lettersMoney[2]){
+				var moneyLetter2 = document.getElementById("div2");
+				moneyLetter2.innerHTML = "n";
+				letterCounter++;
+			}
+
+			else if (userGuess === lettersMoney[3]){
+				var moneyLetter3 = document.getElementById("div3");
+				moneyLetter3.innerHTML = "e";
+				letterCounter++;
+			}
+			else if (userGuess === lettersMoney[4]){
+				var moneyLetter4 = document.getElementById("div4");
+				moneyLetter4.innerHTML = "y";
+				letterCounter++;
+			}
+
+		 	else if (userGuess !== lettersMoney[i]){
+		 		incorrectGuess++;
+		 		lives--;
+		 	}
+
+		}
+		 	if (letterCounter === 5){
+		 		wins++;
+		 		reset();
+		 	}
+
+		 	if (lives = 0){
+		 		losses++;
+		 		reset();
+		 	}
+		}
+	}
 }
-	
-	 
-
-	if (generatedWord === words[1]){
-	 	var lettersRadio= ["r", "a", "d", "i", "o"];
-	 	for(i = 0; i < lettersRadio.length; i++) {
-		 		console.log(lettersRadio[i]);
-
-		if (userGuess === lettersRadio[0]){
-			var radioLetter0 = document.getElementById("div0");
-			radioLetter0.innerHTML = "r";
-		}
-		else if (userGuess === lettersRadio[1]){
-			var radioLetter1 = document.getElementById("div1");
-			radioLetter1.innerHTML = "a";
-		}
-
-		else if (userGuess === lettersRadio[2]){
-			var radioLetter2 = document.getElementById("div2");
-			radioLetter2.innerHTML = "d";
-		}
-
-		else if (userGuess === lettersRadio[3]){
-			var radioLetter3 = document.getElementById("div3");
-			radioLetter3.innerHTML = "i";
-		}
-		else if (userGuess === lettersRadio[4]){
-			var radioLetter4 = document.getElementById("div4");
-			radioLetter4.innerHTML = "o";
-			letterCounter++;
-		}
-	 	else if (userGuess !== lettersRadio[i]){
-	 		incorrectGuess++;
-	 		lives--;
-	 	}
-
-	}
-	 	if (letterCounter === 5){
-	 		wins++;
-	 		reset();
-	 	}
-
-	 	if (lives = 0){
-	 		losses++;
-	 		reset();
-	 	}
-}
-
-	if (generatedWord === words[2]){
-	 	var lettersMouse = ["m", "o", "u", "s", "e"];
-	 	for(i = 0; i < lettersMouse.length; i++) {
-		 		console.log(lettersMouse[i]);
-		 		letterCounter++;
-
-		if (userGuess === lettersMouse[0]){
-			var mouseLetter0 = document.getElementById("div0");
-			mouseLetter0.innerHTML = "m";
-			letterCounter++;
-		}
-		else if (userGuess === lettersMouse[1]){
-			var mouseLetter1 = document.getElementById("div1");
-			mouseLetter1.innerHTML = "0";
-			letterCounter++;
-		}
-
-		else if (userGuess === lettersMouse[2]){
-			var mouseLetter2 = document.getElementById("div2");
-			mouseLetter2.innerHTML = "u";
-			letterCounter++;
-		}
-
-		else if (userGuess === lettersMouse[3]){
-			var mouseLetter3 = document.getElementById("div3");
-			mouseLetter3.innerHTML = "s";
-			letterCounter++;
-		}
-		else if (userGuess === lettersMouse[4]){
-			var mouseLetter4 = document.getElementById("div4");
-			mouseLetter4.innerHTML = "e";
-			letterCounter++;
-		}
-
-	 	else if (userGuess !== lettersMouse[i]){
-	 		incorrectGuess++;
-	 		lives--;
-	 	}
-
-	 }
-
-	 	if (letterCounter === 5){
-	 		wins++;
-	 		reset();
-	 	}
-
-	 	if (lives = 0){
-	 		losses++;
-	 		reset();
-	 	}
-	}
-
-	if (generatedWord === words[3]){
-	 	var lettersMoney = ["m", "o", "n", "e", "y"];
-	 	for(i = 0; i < lettersMoney.length; i++) {
-		 		console.log(lettersMoney[i]);
-
-		if (userGuess === lettersMoney[0]){
-			var moneyLetter0 = document.getElementById("div0");
-			moneyLetter0.innerHTML = "m";
-			letterCounter++;
-
-		}
-		else if (userGuess === lettersMoney[1]){
-			var moneyLetter1 = document.getElementById("div1");
-			moneyLetter1.innerHTML = "o";
-			letterCounter++;
-		}
-
-		else if (userGuess === lettersMoney[2]){
-			var moneyLetter2 = document.getElementById("div2");
-			moneyLetter2.innerHTML = "n";
-			letterCounter++;
-		}
-
-		else if (userGuess === lettersMoney[3]){
-			var moneyLetter3 = document.getElementById("div3");
-			moneyLetter3.innerHTML = "e";
-			letterCounter++;
-		}
-		else if (userGuess === lettersMoney[4]){
-			var moneyLetter4 = document.getElementById("div4");
-			moneyLetter4.innerHTML = "y";
-			letterCounter++;
-		}
-
-	 	else if (userGuess !== lettersMoney[i]){
-	 		incorrectGuess++;
-	 		lives--;
-	 	}
-
-	}
-	 	if (letterCounter === 5){
-	 		wins++;
-	 		reset();
-	 	}
-
-	 	if (lives = 0){
-	 		losses++;
-	 		reset();
-	 	}
-	}
-}
-}
-
+// reset function
 function reset(){
   		displayWord = "";
   		correctLetters =[];
   		var incorrectGuess = 0;
 		var lives = 6;
   	}
+
+// Game Score Stats
 var updateWins = '<p>Wins: ' + wins +'</p>';
 	var updateLosses = '<p>Losses: ' + losses +'</p>';
 	var updateincorrectGuess = '<p>Number of Incorrect Guesses: ' + incorrectGuess +'</p>';
